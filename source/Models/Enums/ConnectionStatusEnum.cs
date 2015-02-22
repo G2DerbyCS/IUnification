b546127e7dc95b8a80cf53b48345a3bcbe61d1af
Sam Masamvi <tfsmasamvi@gmail.com>
2015-02-22 01:25:55 +0000

    Implimented INetworkedUnificationPlugin interface.

---
+++ source/Models/Enums/ConnectionStatusEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace IUnification.Models.Enums
+{
+    /// <summary>
+    /// An enum for use with an INetworkedUnificationPlugin implementation to denote plugin to server connection status.
+    /// </summary>
+    public enum ConnectionStatus
+    {
+        Disconnected = 0,
+        Connected    = 1
+    }
+}
---
+++ source/Models/Interfaces/INetworkedUnificationPlugin.cs
@@ -0,0 +1,21 @@
+﻿using IUnification.Models.Enums;
+using System;
+
+namespace IUnification.Models.Interfaces
+{
+    /// <summary>
+    /// An interface for use in implimentation of a Unification plugin with network dependancy.
+    /// </summary>
+    public interface INetworkedUnificationPlugin : IUnificationPlugin
+    {
+        /// <summary>
+        /// Indicates this INetworkedUnificationPlugin instance's connection status.
+        /// </summary>
+        ConnectionStatus ConnectionStatus { get; }
+
+        /// <summary>
+        /// Event to be raised when the plugin to server connection status changes.
+        /// </summary>
+        event EventHandler<ConnectionStatus> ConnectionStatusChangedEvent;
+    }
+}
