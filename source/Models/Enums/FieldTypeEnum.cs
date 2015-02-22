ff5b9cc5e979bd8bdb1352b9e5ec25c73b5ad1a8
Sam Masamvi <tfsmasamvi@gmail.com>
2015-02-22 12:09:39 +0000

    InputFeild now more flexible.
    Chnaged IsSensitive bool property in InputFeild class to FieldType
    FieldType property.
    
    InputField now supports being used as a boolean input field.

---
+++ source/Models/Enums/FieldTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace IUnification.Models.Enums
+{
+    /// <summary>
+    /// An enum for use with an InputFeild instance to denote how the field should be handled in the user interface.
+    /// </summary>
+    public enum FieldType
+    {
+        Boolean   = 1,
+        Standard  = 0,
+        Sensitive = 2
+    }
+}
--- source/Models/InputField.cs
+++ source/Models/InputField.cs
@@ -1,4 +1,5 @@
-﻿namespace IUnification.Models
+﻿using IUnification.Models.Enums;
+namespace IUnification.Models
 {
     /// <summary>
     /// A class representitive of an input field.
@@ -15,10 +16,10 @@
         /// </summary>
         /// <param name="FieldName">Field Title/Name.</param>
         /// <param name="IsSensitive">Field sensitivity.</param>
-        public InputField(string FieldName, bool IsSensitive)
+        public InputField(string FieldName, FieldType FieldType)
         {
             this.FieldName = FieldName;
-            Sensitive      = IsSensitive;
+            this.FieldType = FieldType;
         }
 
         /// <summary>
@@ -31,9 +32,9 @@
         }
 
         /// <summary>
-        /// Indicates if the content of the field should be considered as sensitive.
+        /// Indicates how the field should be handled in the user interface.
         /// </summary>
-        public bool Sensitive
+        public FieldType FieldType
         {
             private set;
             get;
@@ -46,7 +47,27 @@
         {
             set
             {
-                _Value = value;
+                switch (FieldType)
+                {
+                    case FieldType.Boolean:
+                        if (value.ToLowerInvariant().Equals("t") ||
+                            value.ToLowerInvariant().Equals("true"))
+                        {
+                            _Value = "true";
+                        }
+                        else
+                        {
+                            _Value = "false";
+                        }
+
+                        break;
+
+                    case FieldType.Sensitive:
+                    case FieldType.Standard:
+                        _Value = value;
+
+                        break;
+                }
             }
 
             get
