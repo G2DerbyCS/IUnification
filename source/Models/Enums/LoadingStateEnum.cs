102d948258eadfb1735593322ecae81514f10942
sammasamvi <ekitai.k@gmail.com>
2015-03-12 12:57:44 +0000

    Added EventArgs Classes
    Added LoadingState enum.
    Added LoadingCompletedEventArgs class.
    Added StateChangedEventArgs class.

---
+++ source/Models/Enums/LoadingStateEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace IUnification.Models.Enums
+{
+    /// <summary>
+    /// An enum for use in indicating the state of a loading event.
+    /// </summary>
+    public enum LoadingState
+    {
+        Complete = 1,
+        Faild    = 0,
+        Partial  = 3
+    }
+}
